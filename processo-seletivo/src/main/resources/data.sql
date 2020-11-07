e6a9b45529a4e1c735ec63b3e3de0311add8e0f0
Eder Costa <seikitsi@gmail.com>
2020-11-07 11:38:47 -0400

    Adicionando processo seletivo no banco em memoria h2

---
+++ processo-seletivo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into processo_seletivo (id, titulo, descricao, situacao, dt_inicio_inscricao, dt_encerramento_inscricao) values ('1','Processo seletivo 2020', 'Descrição do processo seletivo 2020', 'NOVO', '2020-11-03' , '2020-11-16');
+insert into etapa (id, titulo, descricao, data, processo_seletivo_id) values ('1', 'Prova de títulos', 'Prova de títulos para comprovação dos requisitos mínimos', '2020-12-10', '1');
+insert into etapa (id, titulo, descricao, data, processo_seletivo_id) values ('2', 'Aula prática', 'Aula prática para comprovaćão das competências', '2020-12-15', '1');
