a818c97ea3407bd6cf0cd0db959df19a9cc33183
Eder Costa <seikitsi@gmail.com>
2021-02-06 19:19:06 -0400

    Minhas inscricoes

--- webapp/src/app/candidato/inscricoes/inscricoes.component.ts
+++ webapp/src/app/candidato/inscricoes/inscricoes.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProcessoSeletivo } from 'src/app/secretaria/models/processo-seletivo';
+import { ProcessoSeletivoService } from 'src/app/secretaria/processo-seletivo/processo-seletivo.service';
+import { ModalConfirmacaoComponent } from 'src/app/shared/modais/modal-confirmacao/modal-confirmacao.component';
+import { ModalPadraoComponent } from 'src/app/shared/modais/modal-padrao/modal-padrao.component';
+import { ModalSucessoComponent } from 'src/app/shared/modais/modal-sucesso/modal-sucesso.component';
+import { NotificacaoService } from 'src/app/shared/notificacao.service';
+import { IInscricao } from '../models/inscricao';
+import { InscricoesService } from './inscricoes.service';
 
 @Component({
   selector: 'app-inscricoes',
@@ -6,10 +15,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscricoes.component.scss']
 })
 export class InscricoesComponent implements OnInit {
+  candidatoId: number = 0;
+  inscricoes: IInscricao[];
 
-  constructor() { }
+  constructor(private notificacaoService: NotificacaoService, private route: ActivatedRoute, private inscricoesService : InscricoesService, private processoService: ProcessoSeletivoService) {
+    this.inscricoes = [];
+   }
 
   ngOnInit(): void {
+    const candidatoId = this.route.snapshot.paramMap.get('id');
+    if (candidatoId) {
+      this.candidatoId = Number.parseInt(candidatoId);
+    }
+    this.carregarInscricoes();
   }
+
+  carregarInscricoes(): void {
+    this.inscricoesService.carregarTodasInscricoes(this.candidatoId).subscribe( res => {
+      if (res.ok && res.body) {
+        this.inscricoes = res.body;
+      }
+    })
+  }
+
+  cancelar(inscricao: IInscricao): void {
+    this.notificacaoService.abrirModal(ModalConfirmacaoComponent, {data: {titulo: 'Deseja cancelar a inscrição ?', mensagem: ''}}).afterClosed().subscribe(cancelar => {
+      if (cancelar === true) {        
+          this.inscricoesService.cancelarInscricao(inscricao).subscribe(res =>{            
+            if (res.body && res.ok) {
+              this.notificacaoService.abrirModal(ModalSucessoComponent, {data: {titulo: 'Inscrição cancelada com sucesso!'}}).afterClosed().subscribe(() => {
+                this.carregarInscricoes();
+              })
+            } else {
+              this.notificacaoService.abrirModal(ModalPadraoComponent, {data: {titulo: 'Atenção!', mensagem: 'Não foi possível efetuar o cancelamento.'}})
+            }
+          })
+      }
+    })
+      
+  }
+
+  
 
 }
--- webapp/src/app/candidato/inscricoes/inscricoes.service.ts
+++ webapp/src/app/candidato/inscricoes/inscricoes.service.ts
@@ -1,9 +1,12 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { observable } from "rxjs";
+import { Observable, observable } from "rxjs";
 import { UtilService } from "src/app/shared/util.service";
 import { env } from "src/environments/environment";
 import { IInscricao } from "../models/inscricao";
+
+type ResponseType = HttpResponse<IInscricao>;
+type ArrayResponseType = HttpResponse<IInscricao[]>;
 
 @Injectable({
     providedIn: 'root'
@@ -14,13 +17,21 @@ export class InscricoesService {
     constructor(private http: HttpClient, private utilService: UtilService) { }
 
     inscrever(processoId: number, candidatoId: number) {
-        const inscricao: IInscricao = {'processoSeletivoId': processoId, 'candidatoId': candidatoId}
+        const inscricao: any = {'processoSeletivoId': processoId, 'candidatoId': candidatoId}
         return this.http.post(this.CANDIDATO_URL + `/v1/inscricoes/`, inscricao, {observe: 'response'});        
     }
 
     carregarInscricoesAtivas(processoId: number, candidatoId: number) {
         const params = this.utilService.criarParametrosUrl({'processoSeletivoId': processoId, 'candidatoId': candidatoId})        
         return this.http.get(this.CANDIDATO_URL + `/v1/inscricoes/ativas/candidato`, {params: params, observe: 'response'});
+    }
+
+    carregarTodasInscricoes(candidatoId: number): Observable<ArrayResponseType> {
+        return this.http.get<IInscricao[]>(this.CANDIDATO_URL + `/v1/inscricoes/candidato/${candidatoId}`, {observe: 'response'});
+    }
+
+    cancelarInscricao(inscricao: IInscricao): Observable<ResponseType> {
+        return this.http.put<IInscricao>(this.CANDIDATO_URL + `/v1/inscricoes/`,inscricao, {observe: 'response'});
     }
     
 }
