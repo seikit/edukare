0c24b2205fe71bdafd23dcba1503f1ec927f1803
Eder Costa <seikitsi@gmail.com>
2021-02-07 14:44:50 -0400

    Adicionando microservico escola

---
+++ escola/src/test/java/edukare/escola/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package edukare.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
