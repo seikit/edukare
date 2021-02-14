c4e64e670a73aaf33f3a1df31ff3d29723f744c3
Eder Costa <seikitsi@gmail.com>
2021-02-14 16:50:28 -0400

    Ádicionando token de acesso para acessar recursos dos candidatos.

---
+++ candidato/src/main/java/edukare/candidato/security/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package edukare.candidato.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(authz -> authz
+                .antMatchers("/actuator/health").permitAll()
+                .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+    }
+}
