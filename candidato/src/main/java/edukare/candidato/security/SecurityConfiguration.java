7e02b73d5eea1eb85c134383bca80abb7e952173
Eder Costa <seikitsi@gmail.com>
2021-02-28 09:55:19 -0400

    Ignorando endpoint /h2-console/** do banco em memoria

--- candidato/src/main/java/edukare/candidato/security/SecurityConfiguration.java
+++ candidato/src/main/java/edukare/candidato/security/SecurityConfiguration.java
@@ -2,14 +2,22 @@ package edukare.candidato.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+
             .authorizeRequests(authz -> authz
                 .antMatchers("/actuator/health").permitAll()
                 .anyRequest().authenticated())
--- processo-seletivo/src/main/java/edukare/processoseletivo/security/SecurityConfiguration.java
+++ processo-seletivo/src/main/java/edukare/processoseletivo/security/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package edukare.processoseletivo.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
